960f1a858e0c48cbb33b152098f8dbf9ad371f07
name1 <sasha.e319@gmail.com>
2018-05-21 08:23:35 +0900

    2018/05/21 08:23

--- application/index/controller/UserController.php
+++ application/index/controller/UserController.php
@@ -171,7 +171,62 @@ class UserController extends Controller
         else
         {
             $code = rand(1000, 9999);
-            // session("id", session_create_id());
+            session("mobile_verify_code", $code);
+
+            //sms content
+            $content = "您的区块链公益时间廊注册验证码是\n" . $code;
+
+            // sms config
+            $smsapi = 'http://api.smsbao.com/';
+            $user = 'gdbc';
+            $pass = md5('gdqkl2018'); //短信平台密码
+
+            //send sms
+            $sendurl = $smsapi."sms?u=".$user."&p=".$pass."&m=".$phone_number."&c=".urlencode($content);
+            $result =file_get_contents($sendurl) ;
+            
+            if ($result != 0)
+            {
+                $response['error'] = true;
+            }
+            if (in_array($result, array_keys($send_status)))
+            {
+                $response['text'] = $send_status[$result];
+            } else {
+                $response['text'] = "Unknown Error";
+            }
+            $response['result'] = $result;
+            $response['code'] = $code;
+        }
+        return json_encode($response);
+    }
+
+    public function create_phone_verify_code1(Request $request)
+    {
+        $send_status = [
+            "0" => "短信发送成功",
+            "-1" => "参数不全",
+            "-2" => "服务器空间不支持,请确认支持curl或者fsocket，联系您的空间商解决或者更换空间！",
+            "30" => "密码错误",
+            "40" => "账号不存在",
+            "41" => "余额不足",
+            "42" => "帐户已过期",
+            "43" => "IP地址限制",
+            "50" => "内容含有敏感词"
+        ];
+
+        $phone_number = $request->post('phone_number');
+        $response = [];
+        $response['error'] = false;
+        if (UserModel::where('mobile',$phone_number)->select()->count() == 0)
+        {
+            $response['error'] = true;
+            $response['text'] = "手机号码不存在";
+            return json_encode(["response" => $response]);
+        }
+        else
+        {
+            $code = rand(1000, 9999);
             session("mobile_verify_code", $code);
 
             //sms content
